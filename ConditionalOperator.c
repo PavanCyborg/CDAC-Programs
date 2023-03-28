0fc8c73d2895b01ceb82acd0083aaf43b75bb887
PavanCyborg <pavankasina29@gmail.com>
2023-03-28 21:33:43 +0530

    Update ConditionalOperator.c

--- ConditionalOperator.c
+++ ConditionalOperator.c
@@ -1,3 +1,11 @@
+/*
+Question:
+Develop a C program having following logic. If i is 20 or j is 20, display as “Atleast one variable is having 20” otherwise display “Both variables are not having 20”. If i is less than
+or equal to 40 and j is less than or equal to 40, It should display “Both are less than or equal to 40” otherwise, it should display as “Both are not less than or equal to 40”. Implement this
+using if-else statement as well as with conditional operatorDevelop a C program having following logic. If i is 20 or j is 20, display as “Atleast one
+variable is having 20” otherwise display “Both variables are not having 20”. If i is less than or equal to 40 and j is less than or equal to 40, It should display “Both are less than or equal
+to 40” otherwise, it should display as “Both are not less than or equal to 40”. Implement this using if-else statement as well as with conditional operator.
+*/
 #include<stdio.h>
 int main()
 {
