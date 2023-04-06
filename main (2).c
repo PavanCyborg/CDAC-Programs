dad0143498ec78208df5de73341df7ce0504a300
PavanCyborg <pavankasina29@gmail.com>
2023-04-06 21:02:12 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char str[]="Pavan Kumar";
+    printf("%s\n",str);
+    printf("%ld\n",strlen(str));
+    printf("%ld",sizeof(str));
+
+    return 0;
+}
