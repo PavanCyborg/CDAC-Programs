aca89107a33162fb71ef44f52d743a21ac1a6b1e
PavanCyborg <pavankasina29@gmail.com>
2023-04-09 09:50:51 +0530

    Update Strings_1_Lower_Upper.c

--- Strings_1_Lower_Upper.c
+++ Strings_1_Lower_Upper.c
@@ -18,24 +18,7 @@ void toUpper(char *str2)
 		*(str2+i)=toupper(*(str2+i));
 	}
 	printf("Lower case of str2 is: %s",str2);
-}
-/*char ch;
-	for(ch=48; ch<=57; ch++)
-	{
-		printf("%d is ascii value of %c\n",ch,ch); // Upper case Alphabets
-	}
-	for(ch=65; ch<=90; ch++)
-	{
-		printf("%d is ascii value of %c\n",ch,ch); // lower case alphabets
-	}
-	for(ch=97; ch<=122; ch++)
-	{
-		printf("%d is ascii value of %c\n",ch,ch); // numbers 1-0
-	}
-	/*for(ch=58; ch<=64; ch++)
-	{
-		printf("%d is ascii value of %c\n",ch,ch); // Special characters.
-	}*/
+}	
 int main()
 {
 	char str1[100],str2[100];
@@ -45,11 +28,5 @@ int main()
 	scanf("%s",str2);
 	toLower(str1);
 	toUpper(str2);
-	/*
-	for(i=0;i<strlen(str2);i++)
-	{
-		str2[i]=toupper(str2[i]);
-	}
-	printf("Upper case of str2 is: %s",str2);*/
 	return 0;
 }
